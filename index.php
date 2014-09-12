106bd07be6d2944d707ef85df20c35305838729d
Ruben Castano <rdc9@Njit.edu>
2014-09-12 10:56:27 -0400

    2nd commit

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
 
-echo 'hello world';
+$obj = new another;
+$obj->name = 'steve';
 
+$obj->printHelloWorld();
+print_r($obj);
+class example {
+ public $name;
 
+ 	public function __construct() {
+	 echo 'Hello World...' . $this->name;
+	 }
+	 public function printHelloWorld() {
+	  echo 'Hello world...' . $this->name . "\n";
+	  }
+	  protected function someFunction() {}
 
-
+	  private function someOtherFunction() {}
+	  }
+	  class another extends example {}
 ?>
 
