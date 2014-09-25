7738643a2bbec051f060c50a68de9361b95aa42e
Ruben Castano <rdc9@Njit.edu>
2014-09-25 18:27:50 -0400

    OO version of csv

---
+++ csvtest.php
@@ -0,0 +1,101 @@
+
+
+
+<?php 
+$importer = new csvfile("test.csv",true); 
+while($data = $importer->get(500)) 
+{ 
+print_r($data); 
+} 
+?>
+
+
+<?php
+echo "<html><body><table>\n\n";
+ini_set('display_errors', 'on');
+class csvfile
+{ 	
+	
+    private $fp; 
+    private $parse_header; 
+    private $header; 
+    private $delimiter; 
+    private $length; 
+ 
+    function __construct($filename, $parse_header=false, $delimiter=",", $length=8000) 
+    { 
+		ini_set('auto_detect_line_endings', TRUE);
+        $this->fp = fopen($filename, "r"); 
+        $this->parse_header = $parse_header; 
+        $this->delimiter = $delimiter; 
+        $this->length = $length; 
+        $this->lines = $lines; 
+
+        if ($this->parse_header) 
+        { 
+           $this->header = fgetcsv($this->fp, $this->length, $this->delimiter); 
+        } 
+
+    } 
+ 
+    function __destruct() 
+    { 
+        if ($this->fp) 
+        { 
+            fclose($this->fp); 
+        } 
+    } 
+ 
+    function get($max_lines=0) 
+    { 
+        //if $max_lines is set to 0, then get all the data 
+
+        $data = array(); 
+
+        if ($max_lines > 0) 
+            $line_count = 0; 
+        else 
+            $line_count = -1; // so loop limit is ignored 
+
+        while ($line_count < $max_lines && ($row = fgetcsv($this->fp, $this->length, $this->delimiter)) !== FALSE) 
+        { 
+            if ($this->parse_header) 
+            { 
+                foreach ($this->header as $i => $heading_i) 
+                { 
+				
+                    $row_new[$heading_i] = $row[$i]; 
+                } 
+                $data[] = $row_new; 
+            } 
+            else 
+            { 
+                $data[] = $row; 
+            } 
+
+            if ($max_lines > 0) 
+                $line_count++; 
+        } 
+		foreach($data as $row) {
+			
+			foreach($row as $key => $value) {
+				echo $key . ': ' . $value . "</br> \n";
+			}
+				echo '<br>';
+			}
+			
+			return $data; 
+    }
+
+
+}
+
+
+
+echo "\n</table></body></html>";
+
+?>
+
+
+
+
