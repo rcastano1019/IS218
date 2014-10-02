f32427e3fb0d74d5018d1f29cf946a5d018646c1
Ruben Castano <rdc9@Njit.edu>
2014-10-02 19:30:50 -0400

    cleaned up stuff, working on combining two csv files

--- csvtest.php
+++ csvtest.php
@@ -3,7 +3,9 @@
 
 <?php 
 //hd2013.csv
-$importer = new csvfile("test.csv",true); 
+// "hdtest.csv",
+
+$importer = new csvfile("hd2013.csv", true); 
 while($data = $importer->get(7000)) 
 { 
 print_r($data); 
@@ -16,21 +18,21 @@ class csvfile
 { 	
 	
     private $fp; 
-    private $parse_header; 
+    private $pheader; 
     private $header; 
     private $delimiter; 
     private $length; 
  
-    function __construct($filename, $parse_header=false, $delimiter=",", $length=8000) 
+    function __construct($filename, $pheader=false, $delimiter=",", $length=8000) 
     { 
 		ini_set('auto_detect_line_endings', TRUE);
         $this->fp = fopen($filename, "r"); 
-        $this->parse_header = $parse_header; 
+        $this->pheader = $pheader; 
         $this->delimiter = $delimiter; 
         $this->length = $length; 
         $this->lines = $lines; 
 
-        if ($this->parse_header) 
+        if ($this->pheader) 
         { 
            $this->header = fgetcsv($this->fp, $this->length, $this->delimiter); 
         } 
@@ -46,21 +48,18 @@ class csvfile
     } 
  
     function get($max_lines=0) 
-	
-    { 
-        //if $max_lines is set to 0, then get all the data 
+	{ 
+        
 
         $data = array(); 
 
         if ($max_lines > 0) 
-		
-            //$line_count = 0; 
-        //else 
-            //$line_count = -1; // so loop limit is ignored 
-
-        while ($line_count < $max_lines && ($row = fgetcsv($this->fp, $this->length, $this->delimiter)) !== FALSE) 
+		//if(($filename = fopen("test.csv", "r" )) !== FALSE){
+         
+ 
+        while (($line_count < $max_lines && $row = fgetcsv($this->fp, $this->length, $this->delimiter)) !== FALSE) 
         { 
-            if ($this->parse_header) 
+            if ($this->pheader) 
 			
             { 
 				
@@ -77,9 +76,9 @@ class csvfile
                 $data[] = $row; 
             } 
 
-            //if ($max_lines > 0) 
-             //  $line_count++; 
-			} 
+           
+			}
+			
 			if(empty($_GET)){
 			
 			foreach($data as $row) {
@@ -91,35 +90,22 @@ class csvfile
 		echo '<a href=' . '"http://web.njit.edu/~rdc9/is218/csvtest.php?row=' . $row_num . '"' . '>University ' . $inc . ' </a>';
 	
 		echo '</p>';
-		}}		$row = $data[$_GET['row']];
+		}}
+				$row = $data[$_GET['row']];
 				
 				echo "<table border=1>";
+				
 				foreach($row as $key => $value) {
 					echo "<tr>";
-			echo "<th> $key</th> <td>  $value </td>";
-			echo "</tr>";
+					echo "<th> $key</th> <td>  $value </td>";
+					echo "</tr>";
 			}
 				echo "</table>";
 				
 				//echo '<br>';
 			  
-		}
-		
-		
-
-			
-			
+	}
 }
-
-				/*$t=0;
-				while($t<100{
-				if($t%10==0){
-					echo "<tr>".PHP_EOL;
-					}
-					echo "<td>".$t."</td>".PHP_EOL;
-					$t++;
-					if($t%10==0){
-						echo "</tr>".PHP_EOL;
-						}*/
+	
 ?>
 
